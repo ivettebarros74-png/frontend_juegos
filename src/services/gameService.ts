9bf02e46f372310d6ae7fb86976963f9a385976d
Pablo Yaguar <warspablo@gmail.com>
2026-01-20 22:54:32 -0500

    pequeños cambios

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { Game } from '../types/game.types'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend-juegos.onrender.com/api'
 
 const api = axios.create({
     baseURL: API_URL,
